9af2835de20ad85cb70d1db9638b4bfa237b0de4
Joel Storr <joel.storr@ewe.net>
2023-03-02 13:04:22 +0100

    Started Adding Light Guides

--- src/components/experients.component.jsx
+++ src/components/experients.component.jsx
@@ -1,5 +1,5 @@
 import { useRef } from "react";
-import { OrbitControls, useHelper } from "@react-three/drei";
+import { OrbitControls, PivotControls, useHelper } from "@react-three/drei";
 
 import * as THREE from "three";
 
@@ -13,12 +13,13 @@ export default function Experience() {
   return (
     <>
       //TODO: Comment for Follow Cam
-      {/* <OrbitControls /> */}
+      <OrbitControls />
       <ambientLight intensity={0.05} />
       <directionalLight position={[0, 3, 5]} ref={lightRef} />
       <color args={["#202121"]} attach={"background"} />
       {/* NOTE: Char cube */}
-      <Player />
+      {/* TODO: Uncomment Player */}
+      {/* <Player /> */}
       {/* NOTE: Processor */}
       <mesh position={[0, 0, -2]} castShadow>
         <boxGeometry args={[2, 0.8, 2]} />
@@ -29,6 +30,16 @@ export default function Experience() {
         <boxGeometry args={[50, 0.5, 50]} />
         <meshBasicMaterial color="#020b24" metalness={5} />
       </mesh>
+      <PivotControls>
+        <mesh position={[0, 0, 1]} rotation-x={-(Math.PI * 0.5)}>
+          <planeGeometry args={[1, 1]} />
+          <meshStandardMaterial
+            color={"orange"}
+            emissive={"orange"}
+            emissiveIntensity={5}
+          />
+        </mesh>
+      </PivotControls>
     </>
   );
 }
