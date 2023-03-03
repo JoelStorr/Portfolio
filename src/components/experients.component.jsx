c90c1d37db4a45ff42fc395e0ae2f99cc210429e
Joel Storr <joel.storr@ewe.net>
2023-03-03 10:40:55 +0100

    Added Skills Component

--- src/components/experients.component.jsx
+++ src/components/experients.component.jsx
@@ -1,4 +1,5 @@
 import { useRef } from "react";
+import { useControls } from "leva";
 import { OrbitControls, PivotControls, useHelper } from "@react-three/drei";
 
 import * as THREE from "three";
@@ -6,28 +7,31 @@ import * as THREE from "three";
 //NOTE: My Imports
 import Player from "./player.component";
 import Floor from "./floor.component";
+import Skills from "./skills.component";
 import { Effect } from "./effect.component";
 
 export default function Experience() {
   const lightRef = useRef();
   useHelper(lightRef, THREE.DirectionalLightHelper, 1);
 
+  const { orb } = useControls({ orb: false });
+
   return (
     <>
       //TODO: Comment for Follow Cam
-      <OrbitControls />
+      {orb && <OrbitControls />}
       <Effect />
       <ambientLight intensity={0.05} />
       <directionalLight position={[0, 3, 5]} ref={lightRef} intensity={0.3} />
       <color args={["#202121"]} attach={"background"} />
       {/* NOTE: Char cube */}
-      {/* TODO: Uncomment Player */}
-      <Player />
+      {!orb && <Player />}
       {/* NOTE: Processor */}
       <mesh position={[0, 0, -2]} castShadow>
         <boxGeometry args={[2, 0.8, 2]} />
         <meshStandardMaterial color="darkgray" />
       </mesh>
+      <Skills />
       <Floor />
     </>
   );
---
+++ src/components/skills.component.jsx
@@ -0,0 +1,10 @@
+export default function Skills() {
+  return (
+    <group>
+      <mesh>
+        <boxGeometry args={[0.2, 0.2, 0.2]} />
+        <meshBasicMaterial color={"orange"} />
+      </mesh>
+    </group>
+  );
+}
